3953ab23f74eb737c51f2d349f158cc5e4e227a2
c2-07 <114809705+c2-07@users.noreply.github.com>
2026-07-15 19:23:37 +0530

    feat: enhance local storage configurations and implementations

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,17 +2,31 @@ from pydantic import Field
 from typing import Annotated
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
-    API_BASE_URL: Annotated[str, Field(description="Base API url")]
-    FRONTEND_BASE_URL: Annotated[str, Field(description="Frontend URL")]
-    DATABASE_URL: Annotated[str, Field(description="Base database url")]
-    DEBUG: Annotated[bool, Field(description="Debug Mode (default: False)")]
+    API_BASE_URL: Annotated[str, Field(description="Base API url")] = (
+        "http://localhost:8000"
+    )
+    FRONTEND_BASE_URL: Annotated[str, Field(description="Frontend URL")] = (
+        "http://localhost:8080"
+    )
+    DATABASE_URL: Annotated[str, Field(description="Base database url")] = (
+        "sqlite:///database.db"
+    )
 
-    UPLOAD_DIR: Annotated[str, Field(description="Base directory for saving uploaded files")]
-    MAX_UPLOAD_SIZE: Annotated[int, Field(description="Max allowed filesize to upload")] = 1024 * 1024 * 50 # 50 MB
-    CHUNK_SIZE: Annotated[int, Field(description="Max bytes to read from bytes stream (Uploadfile)")] = 1024 * 1024 * 2 # 2 MB
+    DEBUG: Annotated[bool, Field(description="Debug Mode (default: False)")] = False
+
+    UPLOAD_DIR: Annotated[
+        str, Field(description="Base directory for saving uploaded files")
+    ] = "uploads"
+    MAX_UPLOAD_SIZE: Annotated[
+        int, Field(description="Max allowed filesize to upload")
+    ] = 1024 * 1024 * 50  # 50 MB
+    CHUNK_SIZE: Annotated[
+        int, Field(description="Max bytes to read from bytes stream (Uploadfile)")
+    ] = 1024 * 1024 * 2  # 2 MB
 
     BACKEND_CORS_ORIGINS: str = "*"
 
--- backend/app/core/storage/localstorage.py
+++ backend/app/core/storage/localstorage.py
@@ -1,26 +1,38 @@
 import os
-from typing import AsyncIterable
+from typing import AsyncIterable, AsyncGenerator
 
 import aiofiles
 
 from app.core.storage.base import BaseStorage
+from app.core.config import settings
 
 
 class LocalStorage(BaseStorage):
-    async def upload(self, path: str, stream: AsyncIterable):
-        size = 0
-        async with aiofiles.open(path, "wb") as f:
+    def _get_full_path(self, path: str) -> str:
+        return os.path.join(settings.UPLOAD_DIR, path)
+
+    async def save(self, path: str, stream: AsyncIterable[bytes]):
+        full_path = self._get_full_path(path)
+        os.makedirs(os.path.dirname(full_path), exist_ok=True)
+        async with aiofiles.open(full_path, "wb") as f:
             async for chunk in stream:
                 if not chunk:
                     break
 
                 await f.write(chunk)
-                size += len(chunk)
 
-    def download(self, path: str): ...
+    def get_file(self, path: str):
+        pass
 
-    def delete(self, path: str):
+    async def get_stream(self, path: str) -> AsyncGenerator[bytes, None]:
+        full_path = self._get_full_path(path)
+        async with aiofiles.open(full_path, "rb") as f:
+            while chunk := await f.read(settings.CHUNK_SIZE):
+                yield chunk
+
+    def remove(self, path: str):
+        full_path = self._get_full_path(path)
         try:
-            os.remove(path)
+            os.remove(full_path)
         except FileNotFoundError:
             pass
