c86ffaf3bcacd0073a4a11a4792cca1d8ad72fb9
c2-07 <114809705+c2-07@users.noreply.github.com>
2026-07-21 13:12:00 +0530

    Update backend/app/api/routes/__init__.py

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.routes import file
+from app.api.routes import auth, file
 
 api_router = APIRouter()
 
+api_router.include_router(auth.router)
 api_router.include_router(file.router)
